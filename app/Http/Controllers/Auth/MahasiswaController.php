f54f4dfad757255e8b3c90623315b62c739974ff
atiyaputi46 <atiyaputi46@gmail.com>
2026-03-31 15:23:11 +0700

    controller

---
+++ app/Http/Controllers/Auth/MahasiswaController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class MahasiswaController extends Controller
+{
+    public function index ()
+    { 
+        return "ini adalah method index di MahasiswaController";
+    }
+
+}
